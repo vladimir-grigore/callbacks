22335095617c924aa7fad23756eaa00bc8ba58b7
vagrant <vladimir.grigore@gmail.com>
2017-03-09 19:55:34 +0000

    Finish forEach implementation of the exercise

--- callbacks.js
+++ callbacks.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(parameter){
+    if (parameter === "Waldo") {
+      found(arr.indexOf(parameter));   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
